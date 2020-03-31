0db50ca6fe4ca6fedf4bd04164bae87338b1a4fe
gabriel <code.gabriel@outlook.com>
2020-03-31 09:44:47 +0100

    initial commit

---
+++ OOPProjectApp/OOPProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
